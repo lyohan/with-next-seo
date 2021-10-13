9576e5668e85b63c935621a70bd596c4ff05998f
lyohan <ian_90@likelion.net>
2021-10-13 16:18:26 +0900

    feat: add serverside

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,12 @@
  * that will apply to every page. Full info on how the default works
  * can be found here: https://github.com/garmeeh/next-seo#default-seo-configuration
  */
+import axios from 'axios'
 import { DefaultSeo } from 'next-seo'
-
+import App from 'next/app'
 import SEO from '../next-seo.config'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps, data }) {
   return (
     /* Here we call NextSeo and pass our default configuration to it  */
     <>
@@ -15,4 +16,12 @@ export default function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
     </>
   )
+}
+
+MyApp.getInitialProps = async (appContext) => {
+  // calls page's `getInitialProps` and fills `appProps.pageProps`
+  const appProps = await App.getInitialProps(appContext);
+  const { data } = await axios.get('http://api.icndb.com/jokes/random')
+
+  return { ...appProps, data }
 }
