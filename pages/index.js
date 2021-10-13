6d78a065a61e4527d160ca92d5619ab3bbecf02c
lyohan <ian_90@likelion.net>
2021-10-13 16:42:28 +0900

    feat: update router push

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,17 @@
 import { NextSeo } from 'next-seo'
-import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div>
-      <NextSeo
-        title="Page Meta Title"
-        description="This will be the page meta description"
-        canonical="https://www.canonicalurl.ie/"
-        openGraph={{
-          url: 'https://www.canonicalurl.ie/',
-          title: 'Open Graph Title',
-          description: 'Open Graph Description',
-          images: [
-            {
-              url: 'https://cdn.pixabay.com/photo/2021/08/03/07/03/orange-6518675_960_720.jpg',
-              width: 800,
-              height: 600,
-              alt: 'Og Image Alt',
-            },
-          ],
-        }}
-      />
-      <h1>SEO Added to Page</h1>
+      <h1>라우터 푸쉬!!!!!!!</h1>
       <p>Take a look at the head to see what has been added.</p>
       <p>
         Or checkout how{' '}
-        <Link href="/jsonld">
-          <a>JSON-LD</a>
-        </Link>{' '}
+        <div onClick={() => router.push("/jsonld")}>
+          <p>JSON-LD</p>
+        </div>{' '}
         (Structured Data) is added
       </p>
     </div>
