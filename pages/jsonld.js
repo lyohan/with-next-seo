c5bf39d38e927f46689065a43475affb9f6d5e4b
lyohan <ian_90@likelion.net>
2021-10-13 16:21:48 +0900

    feat: test meta

--- pages/jsonld.js
+++ pages/jsonld.js
@@ -10,9 +10,9 @@ export default function JsonLd() {
     <div>
       <Head>
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="PROJECT LION : 프로젝트라이언" />
+        <meta property="og:title" content="메타태그 바뀌냐?" />
         <meta property="og:url" content="https://projectlion.io/" />
-        <meta property="og:description" content="멋쟁이사자처럼이 만드는 새로운 교육 플랫폼" />
+        <meta property="og:description" content="이건 메타메타메타태그여" />
         <meta property="og:image" content="https://images.unsplash.com/photo-1634057006528-dce9553adee6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80" />
       </Head>
       <h1>JSON-LD Added to Page</h1>
